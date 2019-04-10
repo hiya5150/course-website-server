f7583245a8d29b7560c99d42538ad8d9f8b430d8
cheskyposen <shragaposen@gmail.com>
2019-04-10 00:55:07 -0400

    in middle verify function

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -12,4 +12,20 @@ class Controller {
         // Instantiate model
         return new $model();
     }
+
+    public function verifyAuth($token, $ip){
+        $db = new Database();
+
+        $db->query('SELECT * FROM auth WHERE token = :token');
+        $db->bind(':token', $token);
+        $res = $db->single();
+        // $db->rowCount();
+
+        if($res->token === $token && $res->ip === $ip){
+            $GLOBALS['id'] = $res->student_id;
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
